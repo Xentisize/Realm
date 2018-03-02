76e812ce2c4c822bd7d4bdf42493f9f8764372d3
Xens Chan <xentisize@gmail.com>
2018-03-02 15:29:28 +0800

    Basic structure of Realm v0.0.1

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import textwrap
+
+if __name__ == "__main__":
+    keep_playing = "y"
+    occupants = ["enemy", "friend", "unoccupied"]
+    width = 72
+    dotted_line = "-" * width
+    print(dotted_line)
+    print("\033[1m" + "Attack of The Orcs v0.0.1:" + "\033[0m")
